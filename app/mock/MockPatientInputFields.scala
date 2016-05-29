c7d18adb4bdfeb0a083c1f8a0e3e5af2d4394d0d
Luciana Acevedo <luacevedo90@gmail.com>
2016-05-29 13:39:11 -0300

    Added more info in Preliminary Diagnosis fields call

--- app/mock/MockPatientInputFields.scala
+++ app/mock/MockPatientInputFields.scala
@@ -6,14 +6,15 @@ object MockPatientInputFields {
 
   def getMockedPreliminaryDiagnosisFields(): PreliminaryDiagnosisFieldsResource = {
 
-    val values = List(FieldValue("1", "Si"), FieldValue("2", "No"))
+    val values = List(FieldValue("0", "No"), FieldValue("1", "Si"))
 
-    val pulmonaryEdemaField = InputField(1, "Presenta edema pulmonar?", "EssentialSymptoms.PulmonaryEdema", "select", "combobox", values)
-    val orthopneaField = InputField(2, "Presenta ortopnea?", "EssentialSymptoms.Orthopnoea", "select", "combobox", values)
-    val dyspnoeaField = InputField(3, "Presenta disnea?", "EssentialSymptoms.Dyspnoea", "select", "combobox", values)
+    val pulmonaryEdemaField = InputField(1, "Presenta edema pulmonar?", "EssentialSymptoms.PulmonaryEdema", "boolean", "combobox", values)
+    val orthopneaField = InputField(2, "Presenta ortopnea?", "EssentialSymptoms.Orthopnoea", "boolean", "combobox", values)
+    val dyspnoeaField  = InputField(3, "Presenta disnea?", "EssentialSymptoms.Dyspnoea", "boolean", "combobox", values)
+    val lowerLimbEdemaField = InputField(4, "Presenta edema de miembros inferiores?", "EssentialSymptoms.LowerLimbEdema", "boolean", "combobox", values)
 
     val step1 = StepInputFields(1, List(pulmonaryEdemaField, orthopneaField))
-    val step2 = StepInputFields(2, List(dyspnoeaField))
+    val step2 = StepInputFields(2, List(dyspnoeaField, lowerLimbEdemaField))
 
     PreliminaryDiagnosisFieldsResource(List(step1, step2))
 
